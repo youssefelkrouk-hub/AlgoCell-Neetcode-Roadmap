dbb3370dc2fbecde334e999b6205d143ef712e03
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-15 11:43:43 +0100

    Wihtout using an extra memory O(1)

--- Two_Pointers/Valid_Palindrome/solution.py
+++ Two_Pointers/Valid_Palindrome/solution.py
@@ -1,23 +1,23 @@
-class Solution(object):
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         Cleaned="".join(c.lower() for c in s if c.isalnum())
+#         return Cleaned==Cleaned[::-1]
+        
+# Time coplexity : O(n) ,Space Complexity : O(n) we use an extra memory 
+# Can we do better , solve it with O(1) wihtout using an extra memory just tow pointers pattern !!
+
+
+class Solution:
     def isPalindrome(self, s):
-        l = 0 
-        r = len(s) - 1
-        while l < r : 
-            if not self.alphaNum( s[l] ) :
-                l += 1 
-                continue 
-            if not self.alphaNum( s[r] ) : 
-                r -= 1 
-                continue 
-            if s[l].lower() != s[r].lower() : 
-                return False 
-            l += 1 
-            r -= 1 
-
-        return True 
-
-    def alphaNum( self , c ) : 
-        return (( ord(c) >= ord('a') and ord(c) <= ord('z') ) or
-            ( ord(c) >= ord('A') and ord(c) <= ord('Z') ) or
-            ( ord(c) >= ord('0') and ord(c) <= ord('9') ) )
-         
+        left,right=0,len(s)-1
+        while left<right:
+            if not s[left].isalnum():
+                left+=1
+            elif not s[right].isalnum():
+                right-=1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left+=1
+                right-=1
+        return True
