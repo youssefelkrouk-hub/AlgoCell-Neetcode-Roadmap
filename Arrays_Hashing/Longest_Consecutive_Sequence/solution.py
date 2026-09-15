03695ab3ba170e3cd0d2622354a5a18724a78082
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-15 12:29:24 +0100

    Update

--- Arrays_Hashing/Longest_Consecutive_Sequence/solution.py
+++ Arrays_Hashing/Longest_Consecutive_Sequence/solution.py
@@ -4,11 +4,10 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         num_set = set(nums)
         longest = 0
-
         for n in num_set:
             # Ne commence à compter que si c'est le début d'une séquence
             if (n - 1) not in num_set:
-                length = 1
+                length = 0 
                 while (n + length) in num_set:
                     length += 1
                 longest = max(longest, length)
