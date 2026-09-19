0aa7fff5cdaaaf9cf4d277916d586c82feb80de8
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-19 20:18:06 +0100

    Monotonic Stack Pattern

--- Stack/Next Greater Element I/Monotonic_stack/nge.py
+++ Stack/Next Greater Element I/Monotonic_stack/nge.py
@@ -1,3 +1,20 @@
+print("\n")
+
+def monotonic_decreasing_stack(nums):
+    stack = []
+    for num in nums:
+        # Maintain decreasing order: pop elements <= current num
+        while stack and stack[-1] <= num:
+            stack.pop()
+        stack.append(num)
+    return stack
+
+nums=[1,2,3,4]
+print(monotonic_decreasing_stack(nums))
+# we use this monotonic decreasing stack to implemente the next greater element 
+
+print("\n")
+
 def nge(nums):
     n = len(nums)
     res = [0] * n
@@ -14,3 +31,27 @@ def nge(nums):
 
 nums=[1,3,4,2]
 print(nge(nums))
+
+print("let's implement the Next Smaller Elment usign a Monotonic increasing stack: ")
+
+# the code of an increasing stack : 
+
+
+
+def nse(nums):
+    n=len(nums)
+    stack=[]
+    res=[0]*(len(nums))
+    for i in range(n-1,-1,-1):
+        while stack and stack[-1]>=nums[i]:
+            stack.pop()
+        if stack:
+            res[i]=stack[-1]
+        else:
+            res[i]=-1
+        stack.append(nums[i])
+    return res
+
+print("\n")
+nums_2=[1,3,4,2]
+print(nse(nums_2)) # -->  [-1,2,2,-1] , time complexity is 
