9225a9bd9fe8a7a2252d816174a6db4995e41854
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-19 23:59:28 +0100

    maping each element with his nge

--- Stack/Next Greater Element I/Monotonic_stack/nge.py
+++ Stack/Next Greater Element I/Monotonic_stack/nge.py
@@ -67,10 +67,31 @@ def nge_map(nums):
     for i in range(n-1,-1,-1):
         while stack and stack[-1]<=nums[i]:
             stack.pop()
-        hash_map[nums[i]]=-1 if  not stack else stack[-1]
+        hash_map[nums[i]]=-1 if  not stack else stack[-1] # if the stack is empty we add -1, else we add the top elemnt wich is exactly the next greater element of nums[i] 
         stack.append(nums[i])
 
     return hash_map
 
-L=[1,3,4,2]
+
+
+L=[1,3,4,2]  
 print(nge_map(L))
+
+# nums1 is a subset of nums2 , serachin for the next alment of all elment of nums1 in nums2
+
+## Time Complexity: O(n + m) –---    one pass over nums2 and nums1.
+## Space Complexity: O(n)    –---    stack and nextGreater array. 
+
+def nge_problem(nums1,nums2):
+    hash_map,stack={},[]
+    for i in range(len(nums2)-1,-1,-1):
+        while stack and stack[-1]<=nums2[i]:
+            stack.pop()
+        hash_map[nums2[i]]=-1 if not stack else stack[-1]
+        stack.append(nums2[i])
+    return [hash_map[num] for num in nums1] 
+
+L1 = [4,1,2]
+L2 = [1,5,4,2,8]
+print(nge_problem(L1,L2))
+    
